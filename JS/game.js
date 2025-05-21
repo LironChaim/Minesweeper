1923f3174d20c3f2ab119bdfa683e8c6cedee04c
LironChaim <138761067+LironChaim@users.noreply.github.com>
2025-05-21 05:52:26 +0300

    working on counting mines at Renderboard func

--- JS/game.js
+++ JS/game.js
@@ -11,15 +11,30 @@
 
 // Constants
 // ---------
-const MINE = '⊗';	
-const FLAG = '♦' 
+const MINE = '💣'
+const FLAG = '🚩' 
 const EMPTY = ' '
 
+// globals
+// -------
+ var gLevel = {
+    Beginner :{ SIZE: 4, MINES: 2},
+    Medium :{ SIZE: 4, MINES: 2},
+    'Help Me God' :{ SIZE: 4, MINES: 2},
+}
+
+var gGame = {
+
+    isOn: false,
+    revealedCount:0,
+    markedCount:0,
+    secPassed:0
+}
 
 // The Functionalty
 
  function onInit () {
-    var gBoard = buildBoard()
+    var gBoard = buildBoard() 
     renderBoard(gBoard)
  }
 
@@ -33,7 +48,18 @@ const EMPTY = ' '
         board.push([])
 
         for (let j = 0; j < size; j++) {
-            board[i][j]= EMPTY
+            board[i][j]= {
+
+                minesAroundCount: 0,
+                isRevealed: false,
+                isMine: false,
+                isMarked: false
+            }
+
+            if (board.isRevealed = false) {
+
+            }
+            
             
         }
         
@@ -102,4 +128,17 @@ const EMPTY = ' '
  function renderCell (loction, value) {
     const elCell = document.querySelector(`.cell -${loction.i}-${loction.j}`)
     elCell.innerHTML = value
+ }
+// chaking for Mines near cliked
+ function setMinesNegsCount(board) {
+    for ( var i = 0; i < board.length ; i++ ) {
+        for ( var j = 0; j < board.length; j++) {
+            if (board[i][j]=isMine){
+                board[i][j].minesAroundCount = +1
+            }
+        }
+    }
+
+    
+
  }
